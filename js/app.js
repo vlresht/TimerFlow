8c2f24a37f04fc0b92440a0ab07e3a3c85618a47
vlresht <vlreshtanenko@gmail.com>
2021-05-19 16:49:13 +0300

    initial commit add simple countdown

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const countdownElement = document.querySelector("#countdown");
+    const startButton = document.querySelector("#timer-start");
+    
+    function startTimer() {
+        let time = document.querySelector("#time-input").value * 60;
+        setInterval(function() {
+            if (time <= 0) {
+                clearInterval(time = 0);
+            }
+            const minutes = Math.floor(time / 60);
+            let seconds = time % 60;
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+            countdownElement.innerHTML = `${minutes}:${seconds}`;
+            time--;
+        }, 1000)
+    }
+
+    startButton.addEventListener('click', startTimer);
+})
