b7d7f986b27c49b02b18c58daee233ceed10ebae
vlresht <vlreshtanenko@gmail.com>
2021-05-23 19:25:34 +0300

    add break timer and pause button

--- js/app.js
+++ js/app.js
@@ -1,22 +1,83 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const countdownElement = document.querySelector("#countdown");
-    const startButton = document.querySelector("#start-button");
+var countdownElement = document.querySelector("#countdown");
+var startButton = document.querySelector("#start-button");
+var stopButton = document.querySelector("#stop-button");
+
+var timerStarted;
+
+startButton.addEventListener('click', function() {
     
-    function startTimer() {
-        let time = document.querySelector("#work-time-input").value * 60;
-        setInterval(function() {
-            if (time <= 0) {
-                clearInterval(time = 0);
-            }
-            const minutes = Math.floor(time / 60);
-            let seconds = time % 60;
-            if (seconds < 10) {
-                seconds = "0" + seconds;
-            }
-            countdownElement.innerHTML = `${minutes}:${seconds}`;
-            time--;
-        }, 1000)
+    workMinutes = document.querySelector("#work-time-input").value; 
+    workSeconds = 0;
+    breakMinutes = document.querySelector("#break-time-input").value;
+    breakSeconds = 0;
+
+    workMinutesCopy = workMinutes;
+    workSecondsCopy = workSeconds;
+    breakMinutesCopy = breakMinutes;
+    breakSecondsCopy = breakSeconds;
+
+    if (timerStarted === undefined) {
+        timerStarted = setInterval(startTimer, 1000);
+    }
+    else {
+        alert("already going!");
+    }
+})
+
+
+stopButton.addEventListener('click', function() {
+    stopTimer();
+    timerStarted = undefined;
+})
+
+
+function startTimer() {
+
+    // work
+    if (workSeconds != 0) {
+        workSeconds--;
+        if (workSeconds < 10) {
+            workSeconds = "0" + workSeconds;
+        }
+        countdownElement.innerHTML = `${workMinutes}:${workSeconds}`;
+    }
+    else if(workMinutes != 0 && workSeconds == 0) {
+        workSeconds = 59;
+        workMinutes--;
+        if (workSeconds < 10) {
+            workSeconds = "0" + workSeconds;
+        }
+        countdownElement.innerHTML = `${workMinutes}:${workSeconds}`;
     }
 
-    startButton.addEventListener('click', startTimer);
-})
+    // break
+    if (workMinutes == 0 && workSeconds == 0) {
+        if (breakSeconds != 0) {
+            breakSeconds--;
+            if (breakSeconds < 10) {
+                breakSeconds = "0" + breakSeconds;
+            }
+            countdownElement.innerHTML = `${breakMinutes}:${breakSeconds}`;
+        } else if (breakMinutes != 0 && breakSeconds == 0) {
+            breakSeconds = 59;
+            breakMinutes--;
+            if (breakSeconds < 10) {
+                breakSeconds = "0" + breakSeconds;
+                countdownElement.innerHTML = `${breakMinutes}:${breakSeconds}`;
+            }
+        }
+    }
+
+    // reset
+    if (workMinutes == 0 && workSeconds == 0 && breakMinutes == 0 && breakSeconds == 0) {
+        workMinutes = workMinutesCopy;
+        workSeconds = workSecondsCopy;
+        breakMinutes = breakMinutesCopy;
+        breakSeconds = breakSecondsCopy;
+    }
+}
+
+
+function stopTimer() {
+    clearInterval(timerStarted);
+}
